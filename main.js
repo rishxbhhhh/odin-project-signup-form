94c03424a8faa6dc495f35f55c9e6027b3c85fbd
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-02-03 18:53:16 +0530

    password match error handled along with disabling submit button

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // made by Rishabh Rajpurohit
+var check = function() {
+    var match = false;
+    if (document.getElementById('password').value ==
+      document.getElementById('conf-password').value) {
+        document.getElementsByClassName('message')[0].innerHTML = '';
+        match = true;
+        document.getElementById('btn-submit').disabled = false;
+    } else {
+      document.getElementsByClassName('message')[0].style.color = 'red';
+      document.getElementsByClassName('message')[0].style.fontSize = '10px';
+      document.getElementsByClassName('message')[0].innerHTML = '*password does not match';
+      match = false;
+      document.getElementById('btn-submit').disabled = true;
+    }
+  }
+ 
